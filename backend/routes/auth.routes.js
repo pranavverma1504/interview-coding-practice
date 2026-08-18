13949038005d039a27d83c30705a75f609c6ce77
pranav verma <vermapranav001@gmail.com>
2026-08-18 14:53:24 +0530

    added logout feature

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { login, signup } from "../controllers/auth.controller.js";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/signup", signup);
+router.post("/logout", logout);
+
 
 export default router;
